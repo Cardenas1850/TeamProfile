63cb803e3c1ff5788f271db4a2c085721528f869
Cardenas1850 <99810565+Cardenas1850@users.noreply.github.com>
2022-08-05 00:06:52 -0400

    set up failing employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -8,6 +8,26 @@ const employeeConstructor= new Employee(exampleEmail, exampleId, exampleName);
 
 describe("Employee", () => {
     it('should return a name email and id', () => {
-        expect(exampleEmail.name)
-    })
-})
+        expect(exampleEmail.email).toEqual("Jason");
+        expect(exampleId.id).toEqual("Cardenas7122@gmail.com");
+        expect(exampleName.name).toEqual("1234");
+    });
+});
+
+describe("getEmail", () => {
+    it('should return the employee email', () => {
+        expect(exampleEmail.getEmail()).toEqual("Jason");
+    });
+});
+
+describe("getName", () => {
+    it("should return name", () => {
+        expect(exampleName.getName()).toEqual("1234");
+    });
+});
+
+describe("getId", () => {
+    it("should reutrn id", () => {
+        expect(exampleId.getId().toEqual("Cardenas7122@gmail.com"));
+    });
+});
