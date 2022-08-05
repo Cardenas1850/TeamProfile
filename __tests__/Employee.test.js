46d8824fd9e2109bbe845019ca1377bbdff9e5ba
Cardenas1850 <99810565+Cardenas1850@users.noreply.github.com>
2022-08-05 00:22:15 -0400

    fixed variable orders to get passing tests for employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,31 +3,31 @@ const Employee = require('../lib/Employee');
 let exampleName = "Jason";
 let exampleId = "1234";
 let exampleEmail = "Cardenas7122@gmail.com";
-const employeeConstructor= new Employee(exampleEmail, exampleId, exampleName);
+const employeeConstructor= new Employee(exampleName, exampleId, exampleEmail);
 
 
 describe("Employee", () => {
     it('should return a name email and id', () => {
-        expect(exampleEmail.email).toEqual("Jason");
-        expect(exampleId.id).toEqual("Cardenas7122@gmail.com");
-        expect(exampleName.name).toEqual("1234");
+        expect(employeeConstructor.email).toEqual("Cardenas7122@gmail.com");
+        expect(employeeConstructor.id).toEqual("1234");
+        expect(employeeConstructor.name).toEqual("Jason");
     });
 });
 
 describe("getEmail", () => {
     it('should return the employee email', () => {
-        expect(exampleEmail.getEmail()).toEqual("Jason");
+        expect(employeeConstructor.getEmail()).toEqual("Cardenas7122@gmail.com");
     });
 });
 
 describe("getName", () => {
     it("should return name", () => {
-        expect(exampleName.getName()).toEqual("1234");
+        expect(employeeConstructor.getName()).toEqual("Jason");
     });
 });
 
 describe("getId", () => {
     it("should reutrn id", () => {
-        expect(exampleId.getId().toEqual("Cardenas7122@gmail.com"));
+        expect(employeeConstructor.getId()).toEqual("1234");
     });
 });
