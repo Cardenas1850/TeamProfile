5192cf19ebb7a0e75d323e40dd836d2797185df8
Cardenas1850 <99810565+Cardenas1850@users.noreply.github.com>
2022-08-04 23:52:41 -0400

    setup role files

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,13 @@
 const Employee = require('../lib/Employee');
+
+let exampleName = "Jason";
+let exampleId = "1234";
+let exampleEmail = "Cardenas7122@gmail.com";
+const employeeConstructor= new Employee(exampleEmail, exampleId, exampleName);
+
+
+describe("Employee", () => {
+    it('should return a name email and id', () => {
+        expect(exampleEmail.name)
+    })
+})
