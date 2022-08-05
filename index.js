e31585ce757b6392669bdc17b2c8e92d3f06724d
Cardenas1850 <99810565+Cardenas1850@users.noreply.github.com>
2022-08-05 01:28:02 -0400

    set up questions for manager, engineer, and intern

--- index.js
+++ index.js
@@ -58,47 +58,63 @@ const generateTeam = () => {
 const addInternOrEngineer = () => {
     return inquirer.prompt([
         {
+            type: list,
+            message: "Please select the position of the new employee",
+            name: 'employeeRole',
+            choices: ['Engineer' ,'Intern']
+        },
+
+        {
             type: input,
-            message: ,
-            name:,
+            message: "Please enter the first and last name of the employee",
+            name: 'employeeName',
+        },
+
+        {
+            type: input,
+            message: "Please enter the employees email address",
+            name: "employeeEmail",
+        },
+
+        {
+            type: input,
+            message:  "Please eneter the Employee ID Number" ,
+            name: 'employeeId',
 
         },
         {
             type: input,
-            message: ,
-            name:,
-
+            message: "Please enter the your Engineers Github profile",
+            name: 'engineerGithub',
+            when: answers => answers.employeeRole == 'Engineer'
         },
         {
             type: input,
-            message: ,
-            name:,
+            message: "Please enter the school your Intern is currently attending",
+            name: 'internSchool',
+            when: answers => answers.employeeRole == 'Intern'
 
         },
         {
-            type: input,
-            message: ,
-            name:,
-
-        },
-        {
-            type: input,
-            message: ,
-            name:,
-
-        },
-        {
-            type: input,
-            message: ,
-            name:,
-
-        },
-        {
-            type: input,
-            message: ,
-            name:,
+            type: confirm,
+            message: "Do you have other employees you wish to add?",
+            name: 'addEmployee',
+            default: true
 
         },
 
     ])
-}
+    .then(({employeeRole, employeeName, employeeId, employeeEmail, engineerGithub, internSchool, addEmployee} = addMember) => {
+        if(employeeRole === 'Intern') {
+            team.push(new Intern(employeeName, employeeId, employeeEmail, internSchool));
+        } else if (employeeRole === 'Engineer') {
+            team.push(new Engineer(employeeName, employeeId, employeeEmail, engineerGithub));
+        } if (addEmployee === true) {
+            addInternOrEngineer();
+        } else {
+            console.log(team);
+            let prepareTeam = createCards();
+            generateHTML(prepareTeam);
+        }
+    }
+};
