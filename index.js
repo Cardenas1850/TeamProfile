3a21309f2496330e346b1f126907f63fe6f30cf8
Cardenas1850 <99810565+Cardenas1850@users.noreply.github.com>
2022-08-05 02:01:04 -0400

    updated html generation

--- index.js
+++ index.js
@@ -116,5 +116,45 @@ const addInternOrEngineer = () => {
             let prepareTeam = createCards();
             generateHTML(prepareTeam);
         }
-    }
+    })
 };
+
+
+function generateHTML(cards)
+fs.write('./dist/index.html',
+    `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+    
+    `,
+    (err) => err ? console.error(err) : console.log("The Team profile has succuessfully be genereated to index.html"));
+
+
+    //genereate the employee cards via prompt questions
+    function createCards();
+        let employeeCards = '';
+        let employeeInfo = '';
+        let employeeIcon = '';
+
+        for (i = 0 ; i < team.length; i++) {
+            if(team[i].getRole() == 'Manager') {
+                employeeIcon = `fa regular fa-user`;
+                employeeInfo = `Office Number: ${team[i].officeNumber}`
+            } else if (team[i].getRole() == 'Engineer') {
+                employeeIcon = `fa regular fa-computer`
+                employeeInfo = `Github: <a href="https://github.com/${team[i].engineerGithub}" target="_blank"> ${team[i].engineerGithub}</a>}`;
+            }
+        }
